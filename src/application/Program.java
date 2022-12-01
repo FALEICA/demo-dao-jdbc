019bffcfc69ae63b7ef84519a4af4f88f1bd7657
FALEICA <fabio.leite.cardoso@gmail.com>
2022-12-01 00:18:15 -0300

    testa comm falha na conection

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,16 @@ package application;
 
 import java.util.Date;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Licros");
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		
-		Seller seller = new Seller(21, "BOB", "bob@gmail.com", new Date(), 3000.00, obj);
 		System.out.println(seller);
 		
 
